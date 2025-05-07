ef06bba754ab1cce8392d46c79f4329434969a76
Emi-H106 <emi.hatano@yahoo.com>
2025-05-07 14:54:01 +0200

    fixing the infinite loop

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,5 +8,18 @@ def factorial(n):
         n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
+if len(sys.argv) != 2:
+    print("Usage: ./factorial.py <non-negative integer>")
+    sys.exit(1)
+
+try:
+    num = int(sys.argv[1])
+    if num < 0:
+         print("Error: number must be non-negative")
+        sys.exit(1)
+except ValueError:
+    print("Error: input must be an integer")
+    sys.exit(1)
+
+f = factorial(num)
 print(f)
